1914280cf162ab46f98a9dd58ea871c85cec3562
delaportePL <pierre-louisdelaporte@hotmail.fr>
2022-03-15 17:11:51 +0100

    last commit

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,28 +1,24 @@
 import { useEffect, useState } from "react"
 
 export const ArticleList = () => {
-    const [data , setData] = useState({title : "",body : ""})
+    const [data , setData] = useState([])
     
     useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/posts")
         .then(reponse => reponse.json())
         .then(data => {
-            let top3 = data.slice(0, 3)
-            console.log(top3)
+            let top3 = data.slice(-3)
             setData(top3)
         })
     },[])
 
     
-    return <article className="col-4">
-        <h2>Salut</h2>
-        <ul>
-            { data.map( item => (
-                <li>
-                    <h2>item.title</h2>
-                    <p>item.body</p>
-                </li> 
-            ))}
-        </ul>
-    </article>
+    return <div>
+        { data.map( item => (
+            <article className="col-12">
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+            </article>
+        ))}
+    </div>
 }
